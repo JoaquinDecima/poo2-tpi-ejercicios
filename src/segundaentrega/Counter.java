ed21333acd0eaf64aea1d5307256dfdb358c887d
Pato <joaquin.decima@gmail.com>
2018-03-21 23:14:40 -0300

    Se agrega inicio de guia dos

---
+++ src/segundaentrega/Counter.java
@@ -0,0 +1,15 @@
+package segundaentrega;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import primerentrega.empleado.Empleado;
+
+public class Counter {
+	private List<Integer> numeros = new ArrayList<Integer>();
+	
+	public void addNumber(int i) {
+		this.numeros.add(i);
+	}
+
+}
