d1271d4dd43c670fd8b552f4400e5ee96ed12aaf
Pato <joaquin.decima@gmail.com>
2018-03-25 11:16:02 -0300

    Se termina peticiones de Counter sin TDD

--- src/segundaentrega/Counter.java
+++ src/segundaentrega/Counter.java
@@ -18,24 +18,35 @@ public class Counter {
 		return(num%2==0);
 	}
 	
+	// Retorna Cantidad de Immpares
 	public int getEvenOcurrences() {
 		int count = 0;
 		for(Integer num : this.numeros)
 			if(this.esPar(num)) 
 				count += 1;
-			
-		
 		return(count);
 	}
 	
+	// Retorna cantidad de Pares
 	public int getOddOcurrences() {
 		int count = 0;
 		for(Integer num : this.numeros)
 			if(! this.esPar(num)) 
 				count += 1;
-			
-		
 		return(count);
 	}
-
+	
+	// Retorna True si es numero es multipo de Num
+	private boolean numEsMultiploDe(Integer num, int divisor) {
+		return (num%divisor==0);
+	}
+	
+	// Rertorna la cantidad de numeros multiplos de X
+	public int catDeMultiplosDe(int numero) {
+		int cant = 0;
+		for (Integer num : this.numeros)
+			if (this.numEsMultiploDe(num, numero))
+				cant += 1;
+		return (cant);
+	}
 }
