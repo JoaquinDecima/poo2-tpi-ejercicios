eb551e11ffaa9e1250c18a454016cb919cf8e34c
Pato <joaquin.decima@gmail.com>
2018-04-06 19:55:14 -0300

    Se finaliza ejercicio 2 - Se modifica Test - Se modifican clases

--- src/terceraentrega/productos/ProductoPrimeraNecesidad.java
+++ src/terceraentrega/productos/ProductoPrimeraNecesidad.java
@@ -1,17 +1,19 @@
 package terceraentrega.productos;
 
 public class ProductoPrimeraNecesidad extends Producto {
+	Double descuento = 0.9d;
 
-	public ProductoPrimeraNecesidad(String sNombre, Double dAlgo) {
+	public ProductoPrimeraNecesidad(String sNombre, Double dAlgo, Double dDescuento) {
 		super(sNombre, dAlgo);
+		descuento = (100 - dDescuento) / 100;
 	}
 	
-	public ProductoPrimeraNecesidad(String sNombre, Double dAlgo, boolean bol) {
+	public ProductoPrimeraNecesidad(String sNombre, Double dAlgo, Double dDescuento, boolean bol) {
 		super(sNombre, dAlgo, bol);
-		// TODO Auto-generated constructor stub
+		descuento = (100 - dDescuento) / 100;
 	}
 	
 	public Double getPrecio() {
-		return (this.precio * 0.9);
+		return (this.precio * this.descuento);
 	}
 }
--- test/terceraentrega/ProductoPrimeraNecesidadTest.java
+++ test/terceraentrega/ProductoPrimeraNecesidadTest.java
@@ -8,15 +8,18 @@ import terceraentrega.productos.ProductoPrimeraNecesidad;
 public class ProductoPrimeraNecesidadTest {
 
 	private ProductoPrimeraNecesidad leche;
+	private ProductoPrimeraNecesidad arroz;
 	
 	@Before
 	public void setUp() {
-		leche = new ProductoPrimeraNecesidad("Leche", 8d, false);
+		leche = new ProductoPrimeraNecesidad("Leche", 8d, 10d, false);
+		arroz = new ProductoPrimeraNecesidad("Arroz", 10d, 11d, false);
 	}
 	
 	@Test
 	public void testCalcularPrecio() {
 		assertEquals(new Double(7.2), leche.getPrecio());
+		assertEquals(new Double(8.9), arroz.getPrecio());
 	}
 	
 	
